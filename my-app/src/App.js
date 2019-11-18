9a0e8eb2c691388ddfff0410ec0faced4b62f96c
Tim <timmlaxton@gmail.com>
2019-11-18 16:56:40 +0000

    added components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+      title: 'Take out the bins',
+      completed: false
+     },
+     {
+         id: 2,
+       title: 'Meet Vin',
+       completed: false
+      },
+      {
+        id: 3,
+      title: 'Feed dog',
+      completed: false
+     }
+    ]
+  }
+
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Todos todos={this.state.todos}/>
     </div>
   );
+ }
 }
-
 export default App;
