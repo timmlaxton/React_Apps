c3f3605562b25b0cb94dee93266902531f69c581
Tim <timmlaxton@gmail.com>
2019-11-18 17:27:08 +0000

    added mark complete and on change

--- my-app/src/components/TodoItem.js
+++ my-app/src/components/TodoItem.js
@@ -1,13 +1,33 @@
  import React, { Component } from 'react'
+ import PropTypes from 'prop-types';
 
  export class TodoItem extends Component {
+   getStyle = () => {
+     return {
+       background: '#f4f4f4',
+       padding: "10px",
+       borderBottom: '1px #ccc dotted',
+       textDecoration: this.props.todo.completed ?
+       'line-through' : 'none'
+     }
+   }
+
+   markComplete = (e) => {
+     console.log(this.props);
+   }
+
    render(){
      return (
-       <div>
-       <p>{ this.props.todo.title}</p>
+       <div style={this.getStyle()}>
+       <p>
+       <input type="checkbox" onChange={this.markComplete} /> {' '}
+       { this.props.todo.title}</p>
        </div>
      )
    }
+ }
+ TodoItem.propTypes = {
+   todos: PropTypes.object.isRequired
  }
 
  export default TodoItem
--- my-app/src/components/Todos.js
+++ my-app/src/components/Todos.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import TodoItem from './TodoItem';
-import PropTypes from 'prop-types';
+import PropTypes from  'prop-types'
 
 class Todos extends Component {
   render() {
